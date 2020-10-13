05e6ba3ce2568ddd702aa9b2017e265ec1233489
igor martins <igorm409@gmail.com>
2020-10-13 16:47:21 -0300

    para inserir mais etapas basta apenas fazer os ajustes na pagina etapas  sem a necessidade de mexer na pagina scriptjs

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'fulano de tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: 'vice1.jpg', legenda:' vereador'}
+                
+                ]
+            },
+            {
+                numero: '77222',
+                nome: ' beltrano da silva ',
+                partido: 'abcd',
+                fotos: [
+                    {url: 'vice2.jpg', legenda:'vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'prefeito',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'ciclano',
+                partido: 'abc',
+                vice: 'cic',
+                fotos:[
+                    {url: 'prefeito1.jpg', legenda:'prefeito'},
+                    {url: 'vice3.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero:'84',
+                nome: 'zulano',
+                partido: 'qwrt',
+                vice: 'zul',
+                fotos:[
+                    {url:'prefeito2.jpg', legenda: 'prefeito'},
+                    {url:'vice4.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+
+        ]
+
+    }
+]
